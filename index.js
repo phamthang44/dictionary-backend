2c8f6d03aa7dce5fcbe58284b04001ec0f43735a
phamthang44 <Ducthang4342@gmail.com>
2025-11-27 11:23:14 +0700

    adjust small thing

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import wordRoutes from "./src/routes/word.routes.js";
 import categoryRoutes from "./src/routes/category.routes.js";
 import statisticsRoutes from "./src/routes/statistics.routes.js";
 import { errorHandler } from "./src/middlewares/error.middleware.js";
+import connectToDatabase from "./src/utils/db.js";
 
 dotenv.config();
 const app = express();
@@ -13,16 +14,23 @@ app.use(cors());
 app.use(express.json());
 
 // Connect MongoDB
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
-
+app.use(async (req, res, next) => {
+  try {
+    await connectToDatabase();
+    next();
+  } catch (error) {
+    console.error("Database connection failed:", error);
+    res.status(500).json({ error: "Lỗi kết nối Database" });
+  }
+});
 // Routes
 app.use("/api/categories", categoryRoutes);
 app.use("/api/words", wordRoutes);
 app.use("/api/statistics", statisticsRoutes);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Local Server running on port ${PORT}`));
+}
+export default app;
---
+++ src/utils/db.js
@@ -0,0 +1,46 @@
+// lib/db.js
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGO_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("Chưa cấu hình MONGO_URI trong biến môi trường");
+}
+
+/**
+ * Global variable để cache connection
+ * Trong ES Module, ta gán vào globalThis
+ */
+let cached = globalThis.mongoose;
+
+if (!cached) {
+  cached = globalThis.mongoose = { conn: null, promise: null };
+}
+
+async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      console.log("=> New MongoDB Connection established");
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default connectToDatabase;
